ee1d98315a587bad92b492561c00c3a7e5cc208d
mdshahadatalamm <mdshahadatalam25@gmail.com>
2024-10-19 12:18:43 -0700

    first section

---
+++ src/Components/FirstSection.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { FiSend } from "react-icons/fi";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+import { IoIosArrowDown } from "react-icons/io";
+
+const FirstSection = () => {
+  return (
+    <>
+<section className='firstSection py-3 shadow-md align-items-center'>
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-12">
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <p className='firstHeader'>
+                            <span className='sendIcons'><FiSend /></span>
+                            FREE SHIPPING worldwide for all orders over <span className='dolar'>$150</span>
+                        </p>
+                    </div>
+                    <div className='d-flex align-items-center'>
+                        <p className='firstHeader2'>Track Your Order</p>
+                        <p className='firstHeader2'>English <span><IoIosArrowDown /></span></p>
+                        <p className='firstHeader2'>USD <span><IoIosArrowDown /></span></p>
+                        <div className='d-flex'>
+                            <div><span className='firstContactIcon'><FaFacebookF /></span></div>
+                            <div><span className='firstContactIcon'><FaTwitter /></span></div>
+                            <div><span className='firstContactIcon'><FaInstagram /></span></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+    </>
+  )
+}
+
+export default FirstSection
--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Navber } from './Components/Navber'
 import { Outlet } from 'react-router-dom'
 import { Footer } from './Pages/Footer'
+import FirstSection from './Components/FirstSection'
 
 
 export const RootLayout = () => {
   return (
    <>
+   <FirstSection/>
    <Navber/>
    <Outlet/>
    <Footer/>
