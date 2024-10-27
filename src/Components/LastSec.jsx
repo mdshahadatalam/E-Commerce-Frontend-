568f20e607dbc3662c69acc6bf899b85233fbb96
mdshahadatalamm <mdshahadatalam25@gmail.com>
2024-10-26 17:50:38 -0700

    root

---
+++ src/Components/LastSec.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LastSec = () => {
+  return (
+    <div>LastSec</div>
+  )
+}
--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -3,6 +3,7 @@ import { Navber } from './Components/Navber'
 import { Outlet } from 'react-router-dom'
 import { Footer } from './Pages/Footer'
 import FirstSection from './Components/FirstSection'
+import { LastSec } from './Components/LastSec'
 
 
 export const RootLayout = () => {
